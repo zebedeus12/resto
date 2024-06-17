00e4df52f43081f4fc1348c28aa024fef11cef06
anisanurf27 <116789509+anisanurf27@users.noreply.github.com>
2024-06-17 22:38:57 +0700

    fix error vendor

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>Two</b>Resto',
-    'logo_img' =>  asset('frontend/img/icon-aresto.png'),
+    // 'logo_img' =>  asset('frontend/img/icon-aresto.png'),
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
